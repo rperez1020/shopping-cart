f67801d4a79cf5c232d219ca00a908fcf2111034
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2024-01-02 23:31:16 -0800

    update

--- store.js
+++ store.js
@@ -1,5 +1,11 @@
 import items from "./items.json"
 
+const storeItemTemplate = document.querySelector("#store-item-template")
+
 export function setupStore(){
-    
+    items.forEach(renderStoreItem)
+}
+
+function renderStoreItem(item){
+    const storeItem = storeItemTemplate.content.cloneNode(true)
 }
