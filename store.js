9581669f3f3170925be26b52288744aa11b5e5d5
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2024-01-04 09:20:54 -0800

    update

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 import items from "./items.json"
 
 const storeItemTemplate = document.querySelector("#store-item-template")
+const storeItemContainer = document.querySelector("[data-store-container]")
 
 export function setupStore(){
     items.forEach(renderStoreItem)
@@ -8,4 +9,11 @@ export function setupStore(){
 
 function renderStoreItem(item){
     const storeItem = storeItemTemplate.content.cloneNode(true)
+    const container = storeItem.querySelector("[data-store-item]")
+    container.dataset.itemID = item.id
+
+    const name = storeItem.querySelector('[data-name]')
+    name.innerText = item.name
+
+    storeItemContainer.appendChild(storeItem)
 }
