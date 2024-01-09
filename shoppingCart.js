494ff9939025d7f9db3f43a84d760e350bf687a3
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2024-01-08 23:29:25 -0800

    update

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,6 +1,7 @@
 
 const cartButton = document.querySelector("[data-cart-button]")
 const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper]")
+let shoppingCart = []
 
 
 export function setupShoppingCart(){
@@ -10,3 +11,10 @@ export function setupShoppingCart(){
 cartButton.addEventListener("click", () => {
     cartItemsWrapper.classList.toggle("invisible")
 })
+
+export function addToCart(id){
+    shoppingCart.push({id: id, quantity: 1})
+    console.log(shoppingCart)
+}
+
+// 30:50
